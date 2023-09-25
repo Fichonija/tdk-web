2543ceb97b6dc129aebc5849ed35934543ade5f2
Filip Bolčić <bfilip9205@gmail.com>
2023-09-25 18:19:45 +0200

    feat(contact): add sendContactEmail netlify function

--- netlify/functions/sendContactEmail.ts
+++ netlify/functions/sendContactEmail.ts
@@ -1,10 +1,61 @@
-import type { Handler, HandlerEvent, HandlerContext } from '@netlify/functions';
+import type { Handler, HandlerEvent } from '@netlify/functions';
+import FormData from 'form-data';
+import Mailgun from 'mailgun.js';
 
-const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' }),
-  };
+const key = process.env.MAILGUN_API_KEY as string;
+const domain = process.env.MAILGUN_DOMAIN as string;
+const receiver = process.env.MAILGUN_RECEIVER as string;
+
+interface ContactData {
+  name: string;
+  email: string;
+  message: string;
+}
+
+const handler: Handler = async ({ httpMethod, body }: HandlerEvent) => {
+  if (httpMethod !== 'POST') {
+    return badRequestMethodResponse;
+  }
+
+  if (!body) {
+    return noBodyResponse;
+  }
+
+  const { name, email, message } = JSON.parse(body) as ContactData;
+  if (!(name && email && message)) {
+    return noBodyResponse;
+  }
+
+  const mg = new Mailgun(FormData).client({ username: 'api', key });
+  try {
+    const messageData = {
+      from: `${name} <${email}>`,
+      to: [receiver],
+      subject: 'TDK.hr: Upit',
+      text: message,
+    };
+    const response = await mg.messages.create(domain, messageData);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify((error as Error).message),
+    };
+  }
 };
 
 export { handler };
+
+const badRequestMethodResponse = {
+  statusCode: 400,
+  body: JSON.stringify(`POST request method is required.`),
+};
+
+const noBodyResponse = {
+  statusCode: 400,
+  body: JSON.stringify('Request body must contain parameters: name, email, message.'),
+};
