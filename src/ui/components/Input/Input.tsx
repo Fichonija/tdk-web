df67375f2d5cc629c7e76556c3e0fca4652bbcbf
Filip Bolčić <bfilip9205@gmail.com>
2023-09-29 17:11:08 +0200

    feat(ui-input): extract Label component

--- src/ui/components/Input/Input.tsx
+++ src/ui/components/Input/Input.tsx
@@ -1,14 +1,15 @@
-import { useEffect, useState, type InputHTMLAttributes } from 'react';
+import { type InputHTMLAttributes } from 'react';
+import { Label } from './Label';
 
 type InputAttributes = Pick<InputHTMLAttributes<HTMLInputElement>, 'type' | 'name' | 'required'>;
 
-type Props = InputAttributes & {
+type InputProps = InputAttributes & {
   value: string;
   onChange: (value: string) => void;
   label?: string;
 };
 
-export const Input = ({ label, ...rest }: Props) => {
+export const Input = ({ label, ...rest }: InputProps) => {
   if (label !== undefined) {
     return (
       <div style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
@@ -21,37 +22,7 @@ export const Input = ({ label, ...rest }: Props) => {
   return <InputComponent {...rest} />;
 };
 
-const Label = ({ label, name }: Pick<Props, 'label' | 'name'>) => {
-  //todo replace matching logic with theming ASAP
-  const [matches, setMatches] = useState(false);
-
-  useEffect(() => {
-    const mediaMatch = window.matchMedia('(max-width: 768px)');
-
-    const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
-    mediaMatch?.addEventListener('change', handler);
-    return () => mediaMatch?.removeEventListener('change', handler);
-  }, []);
-
-  return (
-    <label
-      htmlFor={name}
-      style={{
-        fontFamily: 'Montserrat Medium',
-        fontSize: 'var(--m-font-size)',
-        lineHeight: 'var(--m-line-height)',
-        ...(matches && {
-          fontSize: 'var(--r-font-size)',
-          lineHeight: 'var(--r-line-height)',
-        }),
-      }}
-    >
-      {label}
-    </label>
-  );
-};
-
-const InputComponent = ({ value, onChange, ...rest }: Omit<Props, 'label'>) => {
+const InputComponent = ({ value, onChange, ...rest }: Omit<InputProps, 'label'>) => {
   return (
     <input
       value={value}
