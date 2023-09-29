af42612b52b0ec2d0e1f22ef7cfcf775330b9310
Filip Bolčić <bfilip9205@gmail.com>
2023-09-29 16:54:02 +0200

    feat(ui-input): add label prop

--- src/ui/components/Input/Input.tsx
+++ src/ui/components/Input/Input.tsx
@@ -1,13 +1,57 @@
-import type { InputHTMLAttributes } from 'react';
+import { useEffect, useState, type InputHTMLAttributes } from 'react';
 
 type InputAttributes = Pick<InputHTMLAttributes<HTMLInputElement>, 'type' | 'name' | 'required'>;
 
 type Props = InputAttributes & {
   value: string;
   onChange: (value: string) => void;
+  label?: string;
 };
 
-export const Input = ({ value, onChange, ...rest }: Props) => {
+export const Input = ({ label, ...rest }: Props) => {
+  if (label !== undefined) {
+    return (
+      <div style={{ display: 'flex', flexDirection: 'column', gap: '18px' }}>
+        <Label label={label} name={rest.name} />
+        <InputComponent {...rest} />
+      </div>
+    );
+  }
+
+  return <InputComponent {...rest} />;
+};
+
+const Label = ({ label, name }: Pick<Props, 'label' | 'name'>) => {
+  //todo replace matching logic with theming ASAP
+  const [matches, setMatches] = useState(false);
+
+  useEffect(() => {
+    const mediaMatch = window.matchMedia('(max-width: 768px)');
+
+    const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
+    mediaMatch?.addEventListener('change', handler);
+    return () => mediaMatch?.removeEventListener('change', handler);
+  }, []);
+
+  return (
+    <label
+      htmlFor={name}
+      style={{
+        fontFamily: 'Montserrat Medium',
+        fontSize: 'var(--m-font-size)',
+        lineHeight: 'var(--m-line-height)',
+        ...(matches && {
+          fontSize: 'var(--r-font-size)',
+          lineHeight: 'var(--r-line-height)',
+        }),
+      }}
+    >
+      {label}
+    </label>
+  );
+};
+
+const InputComponent = ({ value, onChange, ...rest }: Omit<Props, 'label'>) => {
   return (
     <input
       value={value}
