f5765148b842666ed73a0a68d52da8bdb7b2843f
Filip Bolčić <bfilip9205@gmail.com>
2023-10-01 18:56:00 +0200

    chore: add tailwind integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,11 @@
 import { defineConfig } from "astro/config";
-
 import react from "@astrojs/react";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   server: {
     port: 8080
   },
-  integrations: [react()]
+  integrations: [react(), tailwind()]
 });
