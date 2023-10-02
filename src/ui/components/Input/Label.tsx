ef8e2571b82ddce41812b66971ecc6c951bc2b99
Filip Bolčić <bfilip9205@gmail.com>
2023-10-02 12:20:11 +0200

    feat(ui-label): use tailwind

--- src/ui/components/Input/Label.tsx
+++ src/ui/components/Input/Label.tsx
@@ -1,35 +1,11 @@
-import { useState, useEffect } from 'react';
-
 interface Props {
   label: string;
   name?: string;
 }
 
 export const Label = ({ label, name }: Props) => {
-  //todo replace matching logic with theming ASAP
-  const [matches, setMatches] = useState(false);
-
-  useEffect(() => {
-    const mediaMatch = window.matchMedia('(max-width: 768px)');
-
-    const handler = (e: MediaQueryListEvent) => setMatches(e.matches);
-    mediaMatch?.addEventListener('change', handler);
-    return () => mediaMatch?.removeEventListener('change', handler);
-  }, []);
-
   return (
-    <label
-      htmlFor={name}
-      style={{
-        fontFamily: 'Montserrat Medium',
-        fontSize: 'var(--m-font-size)',
-        lineHeight: 'var(--m-line-height)',
-        ...(matches && {
-          fontSize: 'var(--r-font-size)',
-          lineHeight: 'var(--r-line-height)',
-        }),
-      }}
-    >
+    <label htmlFor={name} className="text-base md:text-xl" style={{ fontFamily: 'Montserrat Medium' }}>
       {label}
     </label>
   );
