72dfafdd863503d853737338d2bb24d279028322
Fichonija <bfilip9205@gmail.com>
2024-01-18 09:53:35 +0100

    MERGE develop to main (#32)
    * [ISSUE-029] Home - max width wrapper  (#31)
    
    fix(page-home): add max width and height wrapper to Hero component
    
    * [ISSUE-006] Colour palette (#33)
    
    feat(ui): add colors to tailwind theme

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,8 +1,10 @@
 import type { Config } from 'tailwindcss';
+import { colors } from './src/ui/theme';
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
+    colors,
     extend: {
       fontFamily: {
         display: 'Montserrat',
