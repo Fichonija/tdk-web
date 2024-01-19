02c7ac228ff0ff38649248e2a6c0d767945acea7
Filip Bolčić <bfilip9205@gmail.com>
2024-01-19 09:26:04 +0100

    refactor(page-home): extract components to separate folder

---
+++ src/modules/HomePage/components/index.ts
@@ -0,0 +1,3 @@
+export { default as Hero } from './Hero.astro';
+export { default as MissionStatement } from './MissionStatement.astro';
+export { default as ProductSection } from './ProductSection';
