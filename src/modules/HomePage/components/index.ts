1c3197b2a308205bb4f47957c51f13d48a49168a
Filip Bolčić <bfilip9205@gmail.com>
2024-01-19 10:35:08 +0100

    feat(page-home): add ContactLinks component

--- src/modules/HomePage/components/index.ts
+++ src/modules/HomePage/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Hero } from './Hero.astro';
 export { default as MissionStatement } from './MissionStatement.astro';
 export { default as ProductSection } from './ProductSection';
+export { default as ContactSection } from './ContactSection';
