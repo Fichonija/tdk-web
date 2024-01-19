d7c4c031cb548272b7210a14f70364422fd1bdb2
Filip Bolčić <bfilip9205@gmail.com>
2024-01-19 11:33:24 +0100

    feat(ui-form): revamp form inputs desing

--- src/shared/form/withFieldLabels.tsx
+++ src/shared/form/withFieldLabels.tsx
@@ -9,7 +9,7 @@ const withFieldLabels = <T extends any>(WrappedComponent: React.FC<T>) => {
 
   const ComponentWithLabels = ({ label, ...rest }: T & FieldLabelProps) => (
     <div className="flex flex-col gap-3">
-      <label htmlFor={(rest as any).name} className="font-display font-medium text-base md:text-xl">
+      <label htmlFor={(rest as any).name} className="font-sansation font-bold text-xl text-gray-800">
         {label}
       </label>
       <WrappedComponent {...(rest as any)} />
