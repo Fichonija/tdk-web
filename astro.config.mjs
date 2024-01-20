72d2eff6df630533a3cc081e3e78b972ff6c61a3
Filip Bolčić <bfilip9205@gmail.com>
2024-01-20 19:18:51 +0100

    chore: add site to astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,12 @@
-import { defineConfig } from "astro/config";
-import react from "@astrojs/react";
-import tailwind from "@astrojs/tailwind";
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 8080
+    port: 8080,
   },
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind()],
+  site: 'https://tdk.hr',
 });
