5a0c0f0192daba723ad86d65a21d252f485e461b
Filip Bolčić <bfilip9205@gmail.com>
2024-01-20 19:40:43 +0100

    chore: configure sitemap for website

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,13 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
+import sitemap from "@astrojs/sitemap";
+
 // https://astro.build/config
 export default defineConfig({
   server: {
-    port: 8080,
+    port: 8080
   },
-  integrations: [react(), tailwind()],
-  site: 'https://tdk.hr',
+  integrations: [react(), tailwind(), sitemap()],
+  site: 'https://tdk.hr'
 });
