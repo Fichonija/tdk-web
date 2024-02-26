3900d7ad72f987f8b75a4c2661495eaa77233cb6
Filip Bolčić <bfilip9205@gmail.com>
2024-02-26 14:42:22 +0100

    feat(component): extract MissionStatement

--- src/modules/HomePage/components/index.ts
+++ src/modules/HomePage/components/index.ts
@@ -1,5 +1,4 @@
 // @ts-nocheck
 export { default as Hero } from './Hero.astro';
-export { default as MissionStatement } from './MissionStatement.astro';
 export { default as ProductSection } from './ProductSection';
 export { default as ContactSection } from './ContactSection';
