5ff83e32235e795a95300c15cbd28baef83ced64
Filip Bolčić <bfilip9205@gmail.com>
2024-03-01 21:38:17 +0100

    perf(page-gazebo): lazy load image in ProductCardImage

--- src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCard/ProductCardImage.tsx
+++ src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCard/ProductCardImage.tsx
@@ -4,5 +4,12 @@ interface Props {
 }
 
 export const ProductCardImage = ({ src, alt }: Props) => (
-  <img src={src} width={400} height={250} className="object-cover bg-white shadow-lg shadow-black/10" alt={alt} />
+  <img
+    src={src}
+    width={400}
+    height={250}
+    className="object-cover bg-white shadow-lg shadow-black/10"
+    alt={alt}
+    loading="lazy"
+  />
 );
