d2470581cf2e208bbd3a2bce8f47353822607485
Filip Bolčić <bfilip9205@gmail.com>
2024-03-02 11:15:55 +0100

    feat(ui-button): add focus, hover, active and disabled styles

--- src/ui/components/Button/Button.tsx
+++ src/ui/components/Button/Button.tsx
@@ -25,17 +25,18 @@ export const Button = ({
 }: Props) => {
   return (
     <Component
-      onClick={onClick}
+      onClick={!isDisabled ? onClick : undefined}
       disabled={isDisabled}
       className={clsx(
         isFullWidth ? 'w-full' : 'w-fit',
-        'py-2 px-4 rounded bg-[#829C9B] disabled:bg-[#afafaf] disabled:cursor-auto',
+        'py-2 px-4 rounded bg-[#829C9B]',
+        isDisabled ? 'bg-[#afafaf] cursor-default' : 'hover:bg-[#688887] focus:bg-[#688887] active:bg-[#3F6362]',
       )}
       {...rest}
     >
-      <div className="group flex items-center gap-2 text-white">
+      <div className={clsx('group flex items-center gap-2 text-white', isDisabled && 'opacity-80')}>
         <span className="font-sansation font-normal text-base">{text}</span>
-        {iconRight && <div className="group-hover:animate-slide">{iconRight}</div>}
+        {iconRight && <div className={clsx(!isDisabled && 'group-hover:animate-slide')}>{iconRight}</div>}
       </div>
     </Component>
   );
