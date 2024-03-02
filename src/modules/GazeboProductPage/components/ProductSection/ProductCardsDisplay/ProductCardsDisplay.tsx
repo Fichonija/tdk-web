e9b2e49ec7d3c2584f037896f600d9a014cbbb47
Filip Bolčić <bfilip9205@gmail.com>
2024-03-02 12:02:11 +0100

    perf(page-gazebo): improve GazeboProductPage html semantics

--- src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCardsDisplay.tsx
+++ src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCardsDisplay.tsx
@@ -14,23 +14,25 @@ export const ProductCardsDisplay = ({ summary, items }: Props) => {
 
   return (
     <div className="w-full flex flex-col gap-8 font-sansation">
-      <ul className="w-fit flex font-light text-2xl text-gray-900">
-        {items.map((item, i) => (
-          <ol
-            tabIndex={0}
-            key={i}
-            className={clsx(
-              'p-4 cursor-pointer border-b-2',
-              selectedItem === item ? 'border-b-black' : 'border-b-black/20',
-            )}
-            onClick={() => setSelectedItem(item)}
-            onKeyDown={(e) => e.key === 'Enter' && setSelectedItem(item)}
-          >
-            {item.title}
-          </ol>
-        ))}
-      </ul>
-      <div className={clsx('flex transition-all duration-300', selectedItem.containerHeight)}>
+      <nav>
+        <ul className="w-fit flex font-light text-2xl text-gray-900">
+          {items.map((item, i) => (
+            <ol
+              tabIndex={0}
+              key={i}
+              className={clsx(
+                'p-4 cursor-pointer border-b-2',
+                selectedItem === item ? 'border-b-black' : 'border-b-black/20',
+              )}
+              onClick={() => setSelectedItem(item)}
+              onKeyDown={(e) => e.key === 'Enter' && setSelectedItem(item)}
+            >
+              {item.title}
+            </ol>
+          ))}
+        </ul>
+      </nav>
+      <article className={clsx('flex transition-all duration-300', selectedItem.containerHeight)}>
         <div className={clsx('py-16 px-8 w-[30%] font-light text-xl text-gray-50 bg-[var(--product-section-color)]')}>
           {summary.map((text, i) => (
             <p key={i}>{text}</p>
@@ -43,7 +45,7 @@ export const ProductCardsDisplay = ({ summary, items }: Props) => {
         ) : (
           <MultipleContentProductCard {...selectedItem} />
         )}
-      </div>
+      </article>
     </div>
   );
 };
