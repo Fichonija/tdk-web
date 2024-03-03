a2410d3f0cef7e2ae341077812acdfd6eed45b6e
Filip Bolčić <bfilip9205@gmail.com>
2024-03-03 14:53:08 +0100

    fix(page-gazebo): fix nav button positioning in MultipleContentProductCard

--- src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCard/MultipleContentProductCard.tsx
+++ src/modules/GazeboProductPage/components/ProductSection/ProductCardsDisplay/ProductCard/MultipleContentProductCard.tsx
@@ -10,29 +10,49 @@ export const MultipleContentProductCard = ({ subitems }: Props) => {
   const { title, text, image } = subitems[currItemIndex];
 
   return (
-    <div className="py-16 px-8 flex-[3] bg-white">
+    <div className="relative p-8 md:p-10 lg:py-16 lg:px-28 flex-[3] bg-white">
       <div className="h-full flex items-center">
-        <ProductNavButton
-          onClick={() => setCurrItemIndex(currItemIndex - 1)}
-          type="left"
-          isDisabled={currItemIndex === 0}
-        />
+        <div className="hidden absolute top-0 left-0 lg:flex w-28 h-full items-center shrink-0">
+          <ProductNavButton
+            onClick={() => setCurrItemIndex(currItemIndex - 1)}
+            type="left"
+            isDisabled={currItemIndex === 0}
+          />
+        </div>
         <div className="w-full self-start flex flex-col items-center gap-8">
           <ProductCardImage src={image.meta.src} alt={image.alt} />
           <div className="flex flex-col items-center gap-4">
-            <h3 className="font-bold text-xl text-gray-800">{title}</h3>
-            <div className="flex flex-col gap-2 font-light text-xl text-gray-800">
+            <div className="max-w-[400px] w-full flex justify-between lg:justify-center">
+              <div className="lg:hidden w-6 h-6">
+                <ProductNavButton
+                  onClick={() => setCurrItemIndex(currItemIndex - 1)}
+                  type="left"
+                  isDisabled={currItemIndex === 0}
+                />
+              </div>
+              <h3 className="font-bold text-xl text-gray-800">{title}</h3>
+              <div className="lg:hidden w-6 h-6">
+                <ProductNavButton
+                  type="right"
+                  onClick={() => setCurrItemIndex(currItemIndex + 1)}
+                  isDisabled={currItemIndex === subitems.length - 1}
+                />
+              </div>
+            </div>
+            <div className="flex flex-col gap-2 font-light text-xl text-gray-800 text-justify lg:text-start">
               {text.map((t) => (
                 <p key={t}>{t}</p>
               ))}
             </div>
           </div>
         </div>
-        <ProductNavButton
-          type="right"
-          onClick={() => setCurrItemIndex(currItemIndex + 1)}
-          isDisabled={currItemIndex === subitems.length - 1}
-        />
+        <div className="hidden absolute top-0 right-0 lg:flex w-28 h-full items-center shrink-0">
+          <ProductNavButton
+            type="right"
+            onClick={() => setCurrItemIndex(currItemIndex + 1)}
+            isDisabled={currItemIndex === subitems.length - 1}
+          />
+        </div>
       </div>
     </div>
   );
