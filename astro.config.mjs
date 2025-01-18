757b51dedcc9d4814dd9ab3772d2f6d663d199be
Fichonija <bfilip9205@gmail.com>
2025-01-18 10:25:37 +0100

    feat(page-gazebo): replace gallery images

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,11 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwind from '@astrojs/tailwind';
 
-import sitemap from "@astrojs/sitemap";
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  server: {
-    port: 8080
-  },
+  server: { port: 8080 },
   integrations: [react(), tailwind(), sitemap()],
-  site: 'https://tdk.hr'
+  site: 'https://tdk.hr',
 });
