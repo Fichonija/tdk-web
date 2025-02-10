c5ed7bd4bcfa267a04e567910abfc546904a7812
Filip Bolčić <filip.bolcic@arsfutura.co>
2025-02-10 11:18:33 +0100

    chore: upgrade tailwind to v4

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
+import tailwindcss from '@tailwindcss/vite';
 
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   server: { port: 8080 },
-  integrations: [react(), tailwind(), sitemap()],
+  integrations: [react(), sitemap()],
+  vite: { plugins: [tailwindcss()] },
   site: 'https://tdk.hr',
 });
